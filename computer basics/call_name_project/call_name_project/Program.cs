abee7a4b1c44c6bd9b924f31942cd88b3f892032
BehradBadiei <behrad.badiei2004@gmail.com>
2023-05-20 17:01:19 +0430

    takalif mabani computer
    takalif C# mabani computer daneshgah apadana shanbeh saat 2 ta 4 o nim

---
+++ computer basics/call_name_project/call_name_project/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace call_name_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("please enter your name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("enter any key to continue...");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ computer basics/integer_multiplication/integer_multiplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace integer_multiplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("please enter a number:");
+            string number = Console.ReadLine();
+            num = int.Parse(number);
+            num *= 3;
+            Console.WriteLine(num);
+            Console.WriteLine("enter any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
